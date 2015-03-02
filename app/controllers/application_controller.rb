59ad02a75854b54dfb40b93af09be5c51ab70348
Ameglian Major <ameglianmajor@gmail.com>
2015-03-01 23:01:24 -0800

    Modified the view not to display save when no factor models are present.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@
 # inherit. In lieu of an errors controller, this controller is being
 # used to handle errors.
 class ApplicationController < ActionController::Base
+
+  def forem_user
+    current_user
+  end
+  helper_method :forem_user
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
