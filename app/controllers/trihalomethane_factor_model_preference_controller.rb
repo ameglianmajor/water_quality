323e3151be68dddcdff739addd206c391ecb9f0d
Ameglian Major <ameglianmajor@gmail.com>
2015-03-08 01:16:47 -0800

    Added the shell of apipie documentation and validation for the trihalomethane factor model. Eventually, a custom validator will be added for this model.

--- app/controllers/trihalomethane_factor_model_preference_controller.rb
+++ app/controllers/trihalomethane_factor_model_preference_controller.rb
@@ -1,6 +1,25 @@
 # This controller allows the user to select which trihalomethane factor
 # models should be displayed.
 class TrihalomethaneFactorModelPreferenceController < ApplicationController
+  api :POST,
+      'trihalomethane_factor_model_preference/update',
+      'Post a set of trihalomethane_factor_models that should be displayed.'
+  description <<-EOS
+    Post a set of trihalomethane_factor_models that should be displayed.
+  EOS
+  #param :trihalomethane_factor_model_preference, Hash, required: true do
+  #  TrihalomethaneFactorModelHelper.columns.each do |column|
+  #    param column.to_sym, TrihalomethaneFactorModelHelper.weight_validator,
+  #    desc: 'The weight that should be ascribed to this contaminant.',
+  #    required: false
+  #  end
+  #  param :name,
+  #  TrihalomethaneFactorModelHelper.model_name,
+  #  desc: "Model's name", required: true
+  #end
+  error 400,
+  'Bad Request. Please check that all parameters were provided and '\
+        'that the request is syntactically correct.'
   def update
     model_ids =
       params['trihalomethane_factor_model_preference'].values.map(&:to_i)
